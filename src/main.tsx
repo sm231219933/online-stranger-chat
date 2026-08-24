bcbaf845bb43bd8062eb5cca30a791cc5d8146f9
sm231219933 <sm231219933@gmail.com>
2026-08-24 12:07:37 +0530

    Keep profile fields focused while typing

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./animations.css";
 import "./fixes.css";
 import "./chrome.css";
+import "./focus-fix";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
