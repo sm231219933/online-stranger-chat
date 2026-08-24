4fdb9af10fe3ed351baaffc38ef0e8029aeefc05
sm231219933 <sm231219933@gmail.com>
2026-08-24 10:07:50 +0530

    Load shared ChatOnlineMe chrome styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./animations.css";
 import "./fixes.css";
+import "./chrome.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
