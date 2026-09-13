97aa797a51cb8d5d936be5c9d8c65c528af498b1
sm231219933 <sm231219933@gmail.com>
2026-09-13 16:04:50 +0530

    Fix input cursor focus loss only

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./animations.css";
 import "./fixes.css";
 import "./chrome.css";
 import "./brand-fix";
+import "./focus-fix";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
